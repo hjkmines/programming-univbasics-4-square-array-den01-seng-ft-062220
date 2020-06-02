0fd41fde2ffe104c5bd17b918f7c00db5247485f
Hyung Kim <hjkmines@gmail.com>
2020-06-02 00:25:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
+  square_array = []
   
+  counter = 0
+  while counter < array.length 
+    square_array.push(array[counter] ** 2)
+    counter += 1
+  end
+  
+  return square_array
 end
